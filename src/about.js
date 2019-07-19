3f6b3092bab9c1f64465bfc7d7ca99e87994803a
Nathan <nathanarohde@gmail.com>
2019-07-19 15:35:40 -0700

    Basic Exercise Finished
    Nathan A Rohde dev-test

--- src/about.js
+++ src/about.js
@@ -1,7 +1,14 @@
 import './scss/about.scss';
+import $ from "jquery";
 
 /**
  * JavaScript code for the about page goes here
- * 
- * 
+ *
+ *
  **/
+
+ $(".race-selection").click(function(){
+   let selectedRace = $(this).attr('id') + '_race'
+   $(".race").removeClass('active');
+   $('#' + selectedRace).addClass('active');
+ })
